31ab1f4fe0f89b6914f10148c67726542e4d1344
Iván Espín <ivanespinchiloeches@gmail.com>
2024-06-13 23:44:19 +0200

    code maturity analysis

--- src/main/java/com/ivan/thehealthsoftwarecompany/hisv3/security/TokenJwtConfig.java
+++ src/main/java/com/ivan/thehealthsoftwarecompany/hisv3/security/TokenJwtConfig.java
@@ -6,17 +6,8 @@ import javax.crypto.SecretKey;
 
 
 public class TokenJwtConfig {
-    public static final SecretKey SECRET_KEY = Jwts.SIG.HS256.key().build();
-    public static final String PREFIX_TOKEN = "Bearer ";
-    public static final String HEADER_AUTHORIZATION = "Authorization";
-    public static final long EXPIRATION_TIME = 1800000;
-    public static final String CONTENT_TYPE_JSON = "application/json";
-    public static final String AUTHORITY = "authority";
-    public static final String AUTHORITIES = "authorities";
     public static final String ROLE_ADMIN = "ROLE_ADMIN";
     public static final String ROLE_USER = "ROLE_USER";
-    public static final String ROLE_SUPERADMIN = "ROLE_SUPERADMIN";
-    public static final String  ROLE_SECRETARY = "ROLE_SECRETARY";
     public static final String  ROLE_EDITOR = "ROLE_EDITOR";
     public static final String  ROLE_CREATOR = "ROLE_CREATOR";
 
